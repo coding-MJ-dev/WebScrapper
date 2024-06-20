e7dcc201d7d4088628ad23876561daa5ec83c91f
MJ <mwjang628@gmail.com>
2024-06-20 22:32:30 +1000

    file import done

--- main.py
+++ main.py
@@ -1,21 +1,44 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, send_file
 from extractors import extract_wanted_jobs
+from file import save_to_file
 # from extractors.berlin import extract_beriln_jobs
 
 app = Flask("JobScrapper")
 
+db = {}
+
+
 @app.route("/")
 def home():
-  return render_template("home.html", name="MJ")
+  return render_template("home.html")
 
 @app.route("/search")
 def hello():
-  print(request.args)
+  # print(request.args)
   keyword = request.args.get("keyword")
-  # jobs =  extract_beriln_jobs(keyword)
-  jobs = extract_wanted_jobs(keyword)
+  print(keyword)
+  if keyword == None or keyword == "":
+    return redirect("/")
+  if keyword in db:
+    jobs = db[keyword]
+  else:
+    # jobs =  extract_beriln_jobs(keyword)
+    jobs = extract_wanted_jobs(keyword)
+    db[keyword] = jobs
 
   return render_template("search.html", keyword = keyword, jobs=jobs)
 
 
+@app.route("/export")
+def export():
+  keyword = request.args.get("keyword")
+  if keyword == None or keyword == "":
+    return redirect("/")
+  if keyword not in db:
+    return redirect(f"/search?keyword={keyword}")
+  save_to_file(keyword, db[keyword])
+  return send_file(f"{keyword}.csv",as_attachment=True)
+
+
 app.run("127.0.0.1")
+# app.run("0.0.0.0")
